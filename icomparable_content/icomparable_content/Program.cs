dbb3bd503fb5058830e0ff7e0c0c40d438ac1b2b
atwof <atwoff@gamil.com>
2022-08-26 20:26:17 -0300

    Leitura de arquivo txt

---
+++ icomparable_content/icomparable_content/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string path = @"/Users/alissonf/Desktop/in.txt";
+
+try
+{
+    using(StreamReader sr = File.OpenText(path))
+    {
+        List<string> list = new List<string>();
+        while(!sr.EndOfStream)
+        {
+            list.Add(sr.ReadLine());
+        }
+
+        list.Sort();
+        foreach (string x in list)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}

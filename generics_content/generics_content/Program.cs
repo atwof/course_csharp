d0f47a1cd0dbd0dc85aa4f45bac69086d8b7d1ab
atwof <atwoff@gamil.com>
2022-08-26 21:37:23 -0300

    Trabalhando com generics

---
+++ generics_content/generics_content/Program.cs
@@ -0,0 +1,14 @@
+﻿using Services;
+
+PrintService printService = new PrintService();
+Console.Write("How many values? ");
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < x; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    printService.AddValue(n);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
---
+++ generics_content/generics_content/Services/PrintService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Services
+{
+    public class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("Print service is full");
+            }
+
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Print service is empty");
+            }
+
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            if(_count == 0)
+            {
+                throw new InvalidOperationException("Print service is null");
+            }
+
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                if (i <= _count - 1)
+                {
+                    Console.Write(_values[i] + ", ");
+                }
+            }
+            Console.Write(_values[_count - 1] + "]");
+            Console.WriteLine();
+        }
+    }
+}
+

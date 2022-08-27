0232e9332220c9e8f1dfc41ca6c9ada5da0eb02b
atwof <atwoff@gamil.com>
2022-08-27 13:54:10 -0300

    Generics com lista, icomparable, vetor, etc

--- generics_content/generics_content/Program.cs
+++ generics_content/generics_content/Program.cs
@@ -1,4 +1,6 @@
-﻿using Services;
+﻿using System.Globalization;
+using Services;
+using Entities;
 
 // PRINT SERVICES
 PrintService<int> printService = new PrintService<int>();
@@ -16,20 +18,22 @@ printService.Print();
 Console.WriteLine("First: " + printService.First());
 
 // MAX
-
-List<int> list = new List<int>();
+Console.WriteLine();
+List<Product> list = new List<Product>();
 
 Console.Write("Enter N: ");
 int n = int.Parse(Console.ReadLine());
 
 for (int i = 0; i < n; i++)
 {
-    x = int.Parse(Console.ReadLine());
-    list.Add(x);
+    string[] vect = Console.ReadLine().Split(",");
+    string name = vect[0];
+    double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+    list.Add(new Product(name, price));
 }
 
 CalculationService calculationService = new CalculationService();
 
-int max = calculationService.Max(list);
+Product max = calculationService.Max(list);
 
 Console.WriteLine("Max: " + max);
--- generics_content/generics_content/Services/CalculationService.cs
+++ generics_content/generics_content/Services/CalculationService.cs
@@ -4,17 +4,17 @@ namespace Services
 {
     public class CalculationService
     {
-        public int Max(List<int> list)
+        public T Max<T>(List<T> list) where T : IComparable
         {
             if(list.Count == 0)
             {
                 throw new ArgumentException("List cannot be empty");
             }
 
-            int max = list[0];
+            T max = list[0];
             for (int i = 0; i < list.Count; i++)
             {
-                if (list[i] > max)
+                if (list[i].CompareTo(max) > 0)
                 {
                     max = list[i];
                 }

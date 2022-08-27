d56630f60b7b18c63717a45d660879bb62c27e88
atwof <atwoff@gamil.com>
2022-08-27 13:38:02 -0300

    Trabalhando com listas, sem generics

--- generics_content/generics_content/Program.cs
+++ generics_content/generics_content/Program.cs
@@ -1,15 +1,35 @@
 ﻿using Services;
 
-PrintService<string> printService = new PrintService<string>();
+// PRINT SERVICES
+PrintService<int> printService = new PrintService<int>();
 
 Console.Write("How many values? ");
 int x = int.Parse(Console.ReadLine());
 
 for (int i = 0; i < x; i++)
 {
-    string n = Console.ReadLine();
-    printService.AddValue(n);
+    int a = int.Parse(Console.ReadLine());
+    printService.AddValue(a);
 }
 
 printService.Print();
 Console.WriteLine("First: " + printService.First());
+
+// MAX
+
+List<int> list = new List<int>();
+
+Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    x = int.Parse(Console.ReadLine());
+    list.Add(x);
+}
+
+CalculationService calculationService = new CalculationService();
+
+int max = calculationService.Max(list);
+
+Console.WriteLine("Max: " + max);
---
+++ generics_content/generics_content/Services/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Services
+{
+    public class CalculationService
+    {
+        public int Max(List<int> list)
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("List cannot be empty");
+            }
+
+            int max = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
+

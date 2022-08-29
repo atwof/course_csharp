7c644903883f6119a790ef657cacbf03e027ae35
atwof <atwoff@gamil.com>
2022-08-29 19:00:26 -0300

    Comparison

---
+++ comparison/comparison/Program.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+List<Product> products = new List<Product>();
+
+products.Add(new Product("TV", 900.00));
+products.Add(new Product("Notebook", 1200.00));
+products.Add(new Product("Tablet", 450.00));
+
+products.Sort();
+
+foreach (Product x in products)
+{
+    Console.WriteLine(x);
+}

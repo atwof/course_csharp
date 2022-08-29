2e09fa232846114cc57dc703e0c1a1257ccfc388
atwof <atwoff@gamil.com>
2022-08-28 21:47:06 -0300

    Trabalhando com extensions

---
+++ extesion_methods/extesion_methods/Extesions/StringExtesions.cs
@@ -0,0 +1,25 @@
+﻿
+namespace System
+{
+    public static class StringExtesions
+    {
+        public static string Cut(this string thisObj, int size)
+        {
+            if (size == 0)
+            {
+                throw new ArgumentException("Tamanho não pode ser zero");
+            }
+
+            if (thisObj.Length <= size)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, size) + "...";
+            }
+
+        }
+    }
+}
+
---
+++ extesion_methods/extesion_methods/Program.cs
@@ -0,0 +1,6 @@
+﻿DateTime dt = new DateTime(2022, 08, 28, 8, 10, 45);
+
+Console.WriteLine(dt.ElapsedTime());
+
+string s1 = "Good morning dear students";
+Console.WriteLine(s1.Cut(10));

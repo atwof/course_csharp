5c342ce3e2514879d9913291ecbbe3b4d88a51b5
atwof <atwoff@gamil.com>
2022-08-28 21:23:04 -0300

    Exercicio com dictionary

--- dictionary/dictionary/Program.cs
+++ dictionary/dictionary/Program.cs
@@ -1,35 +1,80 @@
-﻿
-Dictionary<string, string> cookies = new Dictionary<string, string>();
+﻿// Exercício proposto
 
-cookies.Add("user", "maria");
-cookies.Add("email", "maria@gmail.com");
-cookies.Add("phone", "991171232");
+string path = @"/Users/alissonf/Desktop/in.txt";
+Dictionary<string, int> votes = new Dictionary<string, int>();
 
-Console.WriteLine(cookies["user"]);
-
-if (cookies.ContainsKey("email"))
+/*
+try
 {
-    Console.WriteLine(cookies["email"]);
+    using(StreamReader reader = File.OpenText(path))
+    {
+        while (!reader.EndOfStream)
+        {
+            string[] linha = reader.ReadLine().Split(",");
+            string nome = linha[0];
+            int votos = int.Parse(linha[1]);
+
+            // Veriicar se a chave já existe
+            if (votes.ContainsKey(nome))
+            {
+                int v = votes.GetValueOrDefault(nome);
+                votos += v;
+                votes.Remove(nome);
+                votes.Add(nome, votos);
+
+            }
+            else
+            {
+                votes.Add(nome, votos);
+            }
+        }
+    }
+
+    // Mostrar a quantidade de votos de cada candidato
+    foreach (KeyValuePair<string, int> item in votes)
+    {
+        Console.WriteLine("Candidato: " + item.Key.ToUpper() + " - Votos: " + item.Value);
+    }
 }
-else
+catch(IOException e)
 {
-    Console.WriteLine("There's no key");
+    Console.WriteLine(e.Message);
 }
+*/
 
-cookies.Remove("email");
-if (cookies.ContainsKey("email"))
+try
 {
-    Console.WriteLine(cookies["email"]);
+    using (StreamReader reader = File.OpenText(path))
+    {
+        while (!reader.EndOfStream)
+        {
+            string[] linha = reader.ReadLine().Split(",");
+            string nome = linha[0];
+            int votos = int.Parse(linha[1]);
+
+            // Veriicar se a chave já existe
+            if (votes.ContainsKey(nome))
+            {
+                int v = votes.GetValueOrDefault(nome);
+                votos += v;
+                votes.Remove(nome);
+                votes.Add(nome, votos);
+
+            }
+            else
+            {
+                votes.Add(nome, votos);
+            }
+        }
+    }
+
+    // Mostrar a quantidade de votos de cada candidato
+    foreach (KeyValuePair<string, int> item in votes)
+    {
+        Console.WriteLine("Candidato: " + item.Key.ToUpper() + " - Votos: " + item.Value);
+    }
 }
-else
+catch (IOException e)
 {
-    Console.WriteLine("There's no key");
-}
-
-Console.WriteLine("Size: " + cookies.Count);
-
-Console.WriteLine("ALL COOKIES");
-foreach (KeyValuePair<string, string> x in cookies)
-{
-    Console.WriteLine(x.Key + ":" + x.Value); 
+    Console.WriteLine(e.Message);
 }

1bf3e03c3361ea477c52199eb80ceaad162c648d
atwof <atwoff@gamil.com>
2022-08-29 21:31:08 -0300

    Usando delegate

---
+++ delegates/delegates/Program.cs
@@ -0,0 +1,11 @@
+﻿using Services;
+
+double a = 10;
+double b = 12;
+
+CalculationService.BinaryNumericOperation opMax = CalculationService.Max;
+CalculationService.BinaryNumericOperation opSum = CalculationService.Sum;
+
+Console.WriteLine(opMax(a, b));
+Console.WriteLine(opSum(a, b));
+Console.WriteLine(CalculationService.Square(a));

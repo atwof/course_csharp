d3eabe3398c91b2ca451a6cc4d05ec54cb67f776
atwof <atwoff@gamil.com>
2022-08-29 21:38:11 -0300

    Usando multi cast delegate

---
+++ multiCastDelegate/multiCastDelegate/Program.cs
@@ -0,0 +1,10 @@
+﻿using Services;
+
+double a = 10;
+double b = 12;
+
+CalculationService.BinaryNumerciOperation op = CalculationService.ShowSum;
+op += CalculationService.ShowMax;
+
+op.Invoke(a, b);
+op(a, b);
---
+++ multiCastDelegate/multiCastDelegate/Services/CalculationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Services
+{
+    public class CalculationService
+    {
+        public delegate void BinaryNumerciOperation(double n1, double n2);
+
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
+

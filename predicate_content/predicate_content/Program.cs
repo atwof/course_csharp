5b9d6b10e9a2438f54092eb00a4e451e863cdb9d
atwof <atwoff@gamil.com>
2022-08-30 20:55:59 -0300

    Trabalhando com predicate

---
+++ predicate_content/predicate_content/Program.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+
+List<Product> products = new List<Product>();
+
+products.Add(new Product("TV", 90));
+products.Add(new Product("Notebook", 1200.00));
+products.Add(new Product("Tablet", 350.00));
+
+products.RemoveAll(ProductTest);
+// products.RemoveAll(p => p.Price >= 100);
+
+foreach (Product x in products)
+{
+    Console.WriteLine(x);
+}
+
+static bool ProductTest(Product p)
+{
+    return p.Price >= 100;
+}
+

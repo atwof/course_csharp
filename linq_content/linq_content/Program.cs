2cc4b3f8a384b95da482b0d3287527b1287ddfa5
atwof <atwoff@gamil.com>
2022-08-31 20:45:51 -0300

    Iniciando com linq

---
+++ linq_content/linq_content/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+// Define data source
+int[] x = new int[] { 1, 2, 3, 4, 5 };
+
+// Define the query expression
+var result = x.Where(x => x % 2 == 0)
+              .Select(x => x * 10);
+
+// Execute query
+foreach (int r in result)
+{
+    Console.WriteLine(r);
+}
+

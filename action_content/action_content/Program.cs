3da6644535e99f61398bf4342c306c2b7f36bf11
atwof <atwoff@gamil.com>
2022-08-30 21:05:24 -0300

    Trabalhando com actions

---
+++ action_content/action_content/Program.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("TV", 20));
+list.Add(new Product("Notebook", 2340));
+list.Add(new Product("Tablet", 2220));
+
+Action<Product> actionProduct = UpdatePrice;
+Action<Product> actionP = p => { p.Price += p.Price * 0.1; };
+
+list.ForEach(actionProduct);
+list.ForEach(actionP);
+list.ForEach(p => { p.Price += p.Price * 0.1; });
+
+list.ForEach(UpdatePrice);
+
+foreach (Product p in list)
+{
+    Console.WriteLine(p);
+}
+
+static void UpdatePrice(Product p)
+{
+    p.Price += p.Price * 0.1;
+}
